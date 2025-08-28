8ef93f69f1d7964b0040eca15739bafc6933feac
Mohamed Elawady <mhmdelmetwally22@gmail.com>
2025-08-28 17:26:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let name = prompt("Enter your name: ");
 let gpa = prompt("Enter your GPA: ");
 
-function yourResult(name) {
+function yourResult(name,gpa) {
     let welcome = `Hello ${name}, your result is: `;
     if (gpa < 3) {
         return welcome + " you didn't pass";
