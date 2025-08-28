192c1b84a7408e3936422d68311917e1f1938458
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2025-08-28 04:29:56 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let name = prompt("Enter your name: ");
+let gpa = prompt("Enter your GPA: ");
+
+function yourResult(name) {
+    let welcome = `Hello ${name}, your result is: `;
+    if (gpa < 3) {
+        return welcome + " you didn't pass";
+    } else {
+        return welcome + "Great you passed";
+    }
+}
+console.log(yourResult(name));
